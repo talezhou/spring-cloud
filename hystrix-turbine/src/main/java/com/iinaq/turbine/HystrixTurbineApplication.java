fb26cd99dec73b4180b51ff9024d1ae0ff7e8b34
iinaq <302506562@qq.com>
2019-08-09 14:02:31 +0800

    commit

---
+++ hystrix-turbine/src/main/java/com/iinaq/turbine/HystrixTurbineApplication.java
@@ -0,0 +1,21 @@
+package com.iinaq.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+//开启EnableTurbine
+@EnableTurbine
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixTurbineApplication.class, args);
+    }
+
+}
